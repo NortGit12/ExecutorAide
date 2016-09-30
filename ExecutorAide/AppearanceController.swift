6c741569c11214adb17032539ceae2ce0fa49343
Jeff Norton <jeff12ntn@gmail.com>
2016-09-30 15:11:35 -0600

    Added appearance controller
    This manages the theme styles of the navigation bar and the tab bar

---
+++ ExecutorAide/AppearanceController.swift
@@ -0,0 +1,61 @@
+//
+//  AppearanceController.swift
+//  ExecutorAide
+//
+//  Created by Jeff Norton on 9/30/16.
+//  Copyright © 2016 NortCham. All rights reserved.
+//
+
+import UIKit
+
+class AppearanceController {
+    
+    //==================================================
+    // MARK: - Properties
+    //==================================================
+    
+    // Universal
+    static let fontName = "Avenir Next"
+    static let titleFontSize: CGFloat = 30.0
+    
+    // Navigation bar
+    static let barBackgroundColor = UIColor(red: 44/255.0, green: 43/255.0, blue: 44/255.0, alpha: 1.0)
+    static let navigationItemTextColor = UIColor(red: 255/255.0, green: 255/255.0, blue: 255/255.0, alpha: 1.0)
+    static let navigationItemFontSize: CGFloat = 17.0
+    
+    // TabBar
+    static let tabBarItemFontSize: CGFloat = 16.0
+    
+    // Content
+    static let taskNameFontSize: CGFloat = 20.0
+    static let subTaskNameFontSize: CGFloat = 20.0
+    static let subTaskDescriptorFontSize: CGFloat = 14.0
+    
+    //==================================================
+    // MARK: - Methods
+    //==================================================
+    
+    static func initializeAppearanceDefaults() {
+        
+        // UINavigationBar
+        
+        let titleTextAttributes = [NSFontAttributeName: UIFont(name: AppearanceController.fontName, size: AppearanceController.titleFontSize)!, NSForegroundColorAttributeName: UIColor.white]
+        
+        UINavigationBar.appearance().barTintColor = AppearanceController.barBackgroundColor
+        UINavigationBar.appearance().titleTextAttributes = titleTextAttributes
+        UINavigationBar.appearance().tintColor = .white
+        
+        // TabBar
+        
+        UITabBar.appearance().barTintColor = barBackgroundColor
+        
+        let unselectedTabBarTextColor = UIColor(red: 176/255.0, green: 176/255.0, blue: 185/255.0, alpha: 1.00)
+        let selectedTabBarTextColor = UIColor.white
+        
+        let unselectedTabBarAttributes = [NSFontAttributeName: UIFont(name: AppearanceController.fontName, size: AppearanceController.tabBarItemFontSize)!, NSForegroundColorAttributeName: unselectedTabBarTextColor]
+        let selectedTabBarAttributes = [NSFontAttributeName: UIFont(name: AppearanceController.fontName, size: AppearanceController.tabBarItemFontSize)!, NSForegroundColorAttributeName: selectedTabBarTextColor]
+        
+        UITabBarItem.appearance().setTitleTextAttributes(unselectedTabBarAttributes, for: .normal)
+        UITabBarItem.appearance().setTitleTextAttributes(selectedTabBarAttributes, for: .selected)
+    }
+}
